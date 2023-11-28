fa7105384830a8536d90b2ef4dd86d40f1bc40de
Robertino Garcia <140854280+RobertinoS@users.noreply.github.com>
2023-11-27 23:20:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ a:active {
         <body>
             <h1>Bienvenidos a mi api para el Proyecto Individual Nº1 del Bootcam Henry (Data Science)</h1>
             <p>Mi nombre es Robertino Garcia , alumno de Henry perteneciente al Cohorte Nº 17 (Full-Time)</p>
-            <p><a href="https://pi-ml-steam-acyb.onrender.com/docs" target="_blank">Link Fastapi Steam (/docs) </a></p>
+            <p><a href="https://pi-ml-steam1.onrender.com/docs" target="_blank">Link Fastapi Steam (/docs) </a></p>
             <p><a href="https://github.com/RobertinoS/PI_ML_OPS-Steam" target="_blank">Link del repositorio de GitHub</a></p>
             <p><a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" target="_blank">Mi perfil de Linkedin</a></p>
         </body>
